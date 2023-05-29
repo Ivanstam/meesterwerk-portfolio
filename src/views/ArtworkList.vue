0defd9315d1c55cfd221f52c601105c48d7855b8
Ivan Stam <ivanstam18@gmail.com>
2023-05-29 12:47:41 +0200

    Initial installations, basic homepage
    Installed Vuejs, Vite, tailwindcss, Vuerouter, VueX and dependencies
    Added a basic homepage with keyword search and search by first letter
    Added some routes to make this possible
    Added the store

---
+++ src/views/ArtworkList.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+ <div>
+   Artwork List
+ </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import {computed} from "vue";
+  import store from "../store/index.js";
+
+  const artworks = computed(() => store.state.artwork)
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+</script>
+
+<template>
+  <div class="flex flex-col p-8">
+    <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for artworks"/>
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of alphabet" :key="letter">
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
