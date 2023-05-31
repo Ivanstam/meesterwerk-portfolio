7c14e76ddf584ded7e24ea4d81d87cf54fe09070
Ivan Stam <ivanstam18@gmail.com>
2023-05-31 14:02:37 +0200

    Search by keyword, artworkinfo view
    Search by artist is now able to search for any keyword, name refactoring soon
    Artworkinfo view is now able to detect if the data arrives and is now styled to house some simple data

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,7 +4,7 @@ const key = '0QSmn1vm';
 
 // Fetch the latest 20 artworks with pictures using the artist name and pass to the mutation
 export function searchByArtist({ commit }, artist) {
-    axiosClient.get(`collection?key=${key}&involvedMaker=${artist}&ps=20&imgonly=True`)
+    axiosClient.get(`collection?key=${key}&q=${artist}&ps=20&imgonly=True&s=relevance`)
         .then(({data}) => {
             console.log(data.artObjects );
             commit('setSearchedArtworks', data.artObjects);
@@ -12,10 +12,10 @@ export function searchByArtist({ commit }, artist) {
 }
 
 // Fetch a single artwork using the ID provided in the usual artworks search, pass to mutation
-export function getArtworkInfo({ commit }, id) {
+export async function getArtworkInfo({ commit }, id) {
     axiosClient.get(`collection/${id}?key=${key}`)
         .then(({data}) => {
             console.log(data);
-            commit('setArtworkInfo', data);
+            commit('setArtworkInfo', data.artObject);
         });
 }
--- src/views/ArtworkInfo.vue
+++ src/views/ArtworkInfo.vue
@@ -20,8 +20,20 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <pre>{{ artworkInfo }}</pre>
+  <div v-if="artworkInfo.webImage"
+       class="mx-auto">
+    <h1 class="text-xl font-bold mb-5">{{ artworkInfo.label.title}}</h1>
+    <img :src="artworkInfo.webImage.url" alt="">
+    <div class="italic m-3">
+      {{ artworkInfo.label.description }}
+    </div>
+
+
+    <div class="content-stretch grid grid-cols-1 md:grid-cols-3 mt-2">
+      <div class="text-center"> {{ artworkInfo.principalMaker }}</div>
+      <div class="text-center"> {{ artworkInfo.dating.presentingDate }}</div>
+      <div class="text-center"> {{ artworkInfo.acquisition.method }}</div>
+    </div>
   </div>
 </template>
 
